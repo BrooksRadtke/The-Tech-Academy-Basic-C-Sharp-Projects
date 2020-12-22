d2ffc919bf6bbad568def63a57959bc5ec135d21
Brooks Radtke <brooks@brooksradtke.com>
2020-12-22 11:32:59 -0800

    Push Assignment to Repository

---
+++ Basic C# Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            //AND operator
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            //OR operator
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            //== operator
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            //!= operator
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            //^(XOR) operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C# Programs/QualifiedCarInsurance/QualifiedCarInsurance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualifiedCarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age;
+            bool isDUI;
+            int speedTickets;
+            bool getInsurance;
+
+            //Displays request for the user's age
+            Console.WriteLine("How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            //Asks if user has ever received DUIs
+            Console.WriteLine("Have you ever received a DUI? true or false");
+            isDUI = Convert.ToBoolean(Console.ReadLine());
+
+            //Displays request for number of speeding tickets
+            Console.WriteLine("How many speeding tickets to do you have?");
+            speedTickets = Convert.ToInt32(Console.ReadLine());
+
+            //Checks each variable, Bool NEEDS '==' to function correctly
+            Console.WriteLine("Qualified?");
+            getInsurance = age > 15 && isDUI == false && speedTickets < 4;
+            Console.WriteLine(getInsurance);
+            Console.ReadLine();
+        }
+    }
+}
