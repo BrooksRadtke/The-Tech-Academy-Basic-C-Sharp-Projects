0d79594ecb18b67bd0c08df8cd8e742d1bdf3f63
Brooks Radtke <brooks@brooksradtke.com>
2020-12-21 16:32:52 -0800

    Push Project Progress to Repository

---
+++ Basic C# Programs/Student Daily Report/Student Daily Report/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string myName = Console.ReadLine();
+            //Console.ReadLine();
+            
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
+
+            Console.WriteLine("Do you need help with anything? True or False");
+            string getHelp = Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences that you would like to share?" +
+                " Please give specifics.");
+            string drResponse = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide?" +
+                " Please be specific.");
+            string drFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today");
+            string hoursStudied = Console.ReadLine(); 
+            int myHours = Convert.ToInt32(hoursStudied);
+
+            Console.WriteLine("Thank you for your response."
+                + " An Instructor will respond to you shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C# Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //brief explanation and overview of datatypes
+            bool isStudying = true;
+            byte hoursWorked = 50;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+            
+            decimal moneyInBank = 100.25m;
+            double heightInCentimeters = 211.48193;
+
+            float secondsLeft = 2.122f;
+
+            short temeratureOnMars = -241;
+
+            string myName = "Brooks";
+
+            //convert int to string
+            int currentAge = 28;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = isRaining.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
