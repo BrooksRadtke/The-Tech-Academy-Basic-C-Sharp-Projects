eef98ced261b19d82f36732296dff1407e32e623
Brooks Radtke <brooks@brooksradtke.com>
2020-12-23 10:15:03 -0800

    Push Projects to Repository

---
+++ Basic C# Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            ////Long way of array input
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            ////More efficient way of input
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            ////Efficient shortcut
+            //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+            ////changing values of an index
+            //numArray2[2] = 234987;
+
+            //Console.WriteLine(numArray2[2]);
+            //Console.ReadLine();
+            
+            //List using int
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+            intList.Remove(10);
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C# Programs/ListArrayExercise/ListArrayExercise/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListArrayExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            //array of strings
+            int arrayInput;
+            string[] fruitArray = { "apple", "banana", "cherry", "dates", "kiwi" };
+            Console.WriteLine("Hi there. I have 5 fruit items in an array container. " +
+                "Please enter a number between 0 and 4 to display one:");
+            arrayInput = Convert.ToInt32(Console.ReadLine());
+            
+            //checks if input was within range
+            if(arrayInput <= 4 && arrayInput >= 0)
+            {
+                Console.WriteLine(fruitArray[arrayInput]);
+                Console.ReadLine();
+            }
+            else
+            {
+                //response if answer doesn't have a value
+                Console.WriteLine("Sorry. Please input a number between 0 and 4" +
+                    " to see an item.");
+                arrayInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(fruitArray[arrayInput]);
+                Console.ReadLine();
+            }
+
+            //array of int
+            int arrayInput2;
+            int[] numArray = { 23, 41, 5, 9690, 173029842 };
+            Console.WriteLine("Thank you. Now I have 5 number items in an array container. " +
+                "Please enter a number between 0 and 4 to display one:");
+            arrayInput2 = Convert.ToInt32(Console.ReadLine());
+
+            if(arrayInput2 <= 4 && arrayInput2 >= 0)
+            {
+                Console.WriteLine(numArray[arrayInput2]);
+                Console.ReadLine();
+            }
+            else
+            {
+                //response if answer doesn't have a value
+                Console.WriteLine("Sorry. Please input a number between 0 and 4" +
+                    " to see an item.");
+                arrayInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(numArray[arrayInput2]);
+                Console.ReadLine();
+            }
+
+            //list of strings
+            int listInput;
+            List<string> stringList = new List<string>();
+            stringList.Add("apple");
+            stringList.Add("banana");
+            stringList.Add("cherry");
+            stringList.Add("dates");
+            stringList.Add("kiwi");
+
+            Console.WriteLine("Excellent. Finally, I have 5 fruit items in list container. " +
+                "Please enter a number between 0 and 4 to display one:");
+            listInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(stringList[listInput]);
+            Console.ReadLine();
+        }
+    }
+}
