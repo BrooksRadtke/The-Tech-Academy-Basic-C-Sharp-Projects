157998b8eab61cf900ddbd3a1639205797ca08c5
Brooks Radtke <brooks@brooksradtke.com>
2020-12-23 16:42:04 -0800

    Push progress to repository

--- Basic C# Programs/ConsoleApp/ConsoleApp/Program.cs
+++ Basic C# Programs/ConsoleApp/ConsoleApp/Program.cs
@@ -9,9 +9,9 @@ namespace ConsoleApp
     class Program
     {
         static void Main()
-        {
+        {   //*PART 1*
             //Create a 1-dimensional string[]
-            string[] stringArray = { "apples", "bananas", "cherries", "dates", "kiwi" };
+            string[] stringArray = { "apples - ", "bananas - ", "cherries - ", "dates - ", "kiwi - " };
 
             //Create a loop that iterates through the array and adds
             //user input at the end of each string
@@ -20,7 +20,7 @@ namespace ConsoleApp
             {
                 //ask the user to input text
                 Console.WriteLine("How would you describe fruit? Such as 'apples are...'");
-
+                stringArray[i] = stringArray[i] + Console.ReadLine();
                 
             }
 
@@ -28,6 +28,71 @@ namespace ConsoleApp
             for(int i = 0; i < stringArray.Length; i++)
             {
                 Console.WriteLine(stringArray[i]);
+            }
+            Console.ReadLine();
+
+            //*PART 2*
+            //create an infinite loop
+            bool whileTrue = true;
+            while(whileTrue)                 
+            {
+                //a while loop without a condition to switch to the other state
+                //continues the loop
+                Console.WriteLine("Always true!!");
+                Console.ReadLine();
+                whileTrue = false;
+            }
+            
+            //*PART 3*
+            //Create comparison loop with '<'
+            for(int j = 0; j < 3; j++)
+            {
+                //this comparison loop will print the string 3x
+                Console.WriteLine("And they just keep coming");
+            }
+            Console.ReadLine();
+            //Create comparison loop with '<='
+            //increments the value of 'n' per loop until its value
+            //is <= to 4
+            int n = 0;
+            for(int k = 0; k <= 4; k++ )
+            {
+                n += 1;
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+
+            //PART 4
+            //Create a list of strings where each item is unique
+            string userInput;
+            List<string> fruitList = new List<string> { "apple", "banana", "cherrie", "dates", "kiwi" };
+
+            //asks the user to input text to search the list
+            Console.WriteLine("Please enter the name of a common fruit:");
+
+            //code that loops to check if input item was on list,
+            //if not, continues loop
+            bool gotFruit = false;
+            while(!gotFruit)
+            {
+                //takes user input for searching list
+                userInput = Convert.ToString(Console.ReadLine());
+                for(int i = 0; i < fruitList; i++)
+                {
+                    if (fruitList[i].Contains(userInput))
+                    {
+                        //once true, break the while loop
+                        gotFruit = true;
+                        Console.WriteLine(fruitList);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        //Displays if the user input doesn't match an index string
+                        Console.WriteLine("Sorry, please try a different fruit.");
+                    }
+                }
+                
             }
             Console.ReadLine();
         }
