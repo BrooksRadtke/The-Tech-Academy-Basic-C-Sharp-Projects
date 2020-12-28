1d53de6b0e5391004f460629b5f0bdd5a9b1fce8
Brooks Radtke <brooks@brooksradtke.com>
2020-12-28 11:34:09 -0800

    Push Project to Repository

--- Basic C# Programs/ExceptionHandlingApp/ExceptionHandlingApp/Program.cs
+++ Basic C# Programs/ExceptionHandlingApp/ExceptionHandlingApp/Program.cs
@@ -10,9 +10,35 @@ namespace ExceptionHandlingApp
     {
         static void Main(string[] args)
         {
-            List<int> myList = new List<int> { 2, 6, 14, 25, 58 };
+            try
+            {
+                // Create a list of integers to be divided by
+                List<int> myList = new List<int> { 2, 6, 14, 25, 58 };
+                // Ask the user to enter a number to divide the list by
+                Console.WriteLine("Please enter a number to divide the list by: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int outputNumber;
 
-
+                for (int i = 0; i < myList.Count(); i++)
+                {
+                    outputNumber = myList[i] / userInput;
+                    Console.WriteLine(outputNumber);
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                // Create prompt when user tries to divide by zero
+                Console.WriteLine("Please do not divide by zero!");
+            }
+            catch(FormatException ex)
+            {
+                // Create prompt when user divides by string
+                Console.WriteLine("Sorry, you cannot divide a number by a word...yet.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
