7da7e6fe22b5d97149b94138d62683ae645b9114
Brooks Radtke <brooks@brooksradtke.com>
2020-12-29 14:06:50 -0800

    Push changes to repository

---
+++ Basic C# Programs/StaticMethod/StaticMethod/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Operations
+    {
+        // Create a void method that outputs an int
+        // Data passed into it is divided by 2
+        public void DividedBy(int x)
+        {
+            int product = x / 2;
+            Console.WriteLine(x + " / 2 = " + product);
+        }
+
+        // Create a method without parameters
+        // Overload a method
+        // Declare class static
+        public static void DividedBy()
+        {
+
+        }
+    }
+}
---
+++ Basic C# Programs/StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations obj1 = new Operations();
+
+            Console.WriteLine("Input a number to be divided by 2:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            obj1.DividedBy(x);
+
+            Console.ReadLine();
+        }
+    }
+}
