dd64e3a68934c9296beb68db0307a78d47fd7d3f
Brooks Radtke <brooks@brooksradtke.com>
2020-12-29 09:21:20 -0800

    Push progress to Repository

---
+++ Basic C# Programs/OptParmMethod/OptParmMethod/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptParmMethod
+{
+    class Operations
+    {
+        public int Addition(int x, int y = 1)
+        {
+            int sum = x + y;
+            return sum;
+        }
+    }
+}
--- Basic C# Programs/OptParmMethod/OptParmMethod/Program.cs
+++ Basic C# Programs/OptParmMethod/OptParmMethod/Program.cs
@@ -10,6 +10,21 @@ namespace OptParmMethod
     {
         static void Main()
         {
+            // Instantiate the class
+            Operations obj1 = new Operations();
+
+            // Ask user for input
+            Console.WriteLine("Please enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may enter a second number, or skip by " +
+                "entering nothing:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            // Pass values to method
+            obj1.Addition(x,y);
+
+            Console.WriteLine(x + " + " + y + " = " + obj1.Addition(x,y), obj1.Addition(x, y));
+            Console.ReadLine();
         }
     }
 }
