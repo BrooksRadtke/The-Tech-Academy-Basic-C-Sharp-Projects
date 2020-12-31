c31f097f405474b2d0a29e17d865c135913d82f3
Brooks Radtke <brooks@brooksradtke.com>
2020-12-31 14:05:31 -0800

    Push Project to Repository

---
+++ Basic C# Programs/Structs/Structs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an object of data type Number, and assign amount 
+            // Obj.specification
+            Number myNumber;
+            myNumber.Amount = 9.82m;
+        }
+
+        // Create a struct called Number with a property called Amount as a decimal type
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
