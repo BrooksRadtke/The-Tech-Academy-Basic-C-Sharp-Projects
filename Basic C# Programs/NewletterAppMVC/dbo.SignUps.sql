65f5e697582d72d1eb9021c51404ec08bb182f5e
Brooks Radtke <brooks@brooksradtke.com>
2021-01-07 16:18:15 -0800

    Push progress to repository

---
+++ Basic C# Programs/NewletterAppMVC/dbo.SignUps.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SignUps] (
+    [id]           INT         NOT NULL,
+    [FirstName]    VARCHAR (50)  NULL,
+    [LastName]     VARCHAR (50)  NULL,
+    [EmailAddress] VARCHAR (100) NULL
+);
+
